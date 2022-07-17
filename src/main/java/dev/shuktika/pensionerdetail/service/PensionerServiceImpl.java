6e9dc516bf0c368111d3af7d4ffdd5c5f530924d
Shuktika Mahanty <shuktika.mahanty@gmail.com>
2022-07-17 14:19:27 +0530

    Add Pensioner Repository

--- src/main/java/dev/shuktika/pensionerdetail/service/PensionerServiceImpl.java
+++ src/main/java/dev/shuktika/pensionerdetail/service/PensionerServiceImpl.java
@@ -1,17 +1,21 @@
 package dev.shuktika.pensionerdetail.service;
 
 import dev.shuktika.pensionerdetail.entity.Pensioner;
+import dev.shuktika.pensionerdetail.repository.PensionerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PensionerServiceImpl implements PensionerSevice {
+    private final PensionerRepository pensionerRepository;
     @Override
     public Pensioner savePensioner(Pensioner pensioner) {
-        return null;
+        return pensionerRepository.save(pensioner);
     }
 
     @Override
     public Pensioner fetchPersonByAadharNumber(Long aadharNumber) {
-        return null;
+        return pensionerRepository.findByAadharNumber(aadharNumber).get();
     }
 }
