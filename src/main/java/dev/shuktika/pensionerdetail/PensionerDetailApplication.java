90252a67074a9e15440d86c9c24b92b21369ae63
Shuktika Mahanty <shuktika.mahanty@gmail.com>
2022-07-17 11:38:55 +0530

    Initial commit

---
+++ src/main/java/dev/shuktika/pensionerdetail/PensionerDetailApplication.java
@@ -0,0 +1,13 @@
+package dev.shuktika.pensionerdetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PensionerDetailApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PensionerDetailApplication.class, args);
+    }
+}
